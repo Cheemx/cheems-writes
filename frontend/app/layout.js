3232995a22a8a82b200d4e58a98ca0b5004bca98
Cheemx <chinmaymahan999@gmail.com>
2025-05-26 22:15:38 +0530

    logo updated!

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -22,6 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="@/public/favicon" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
